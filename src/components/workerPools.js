76decc7f68daeaa319eede4dfcbb4febaada6a7d
Rob Thijssen <rthijssen@gmail.com>
2019-11-05 08:22:57 +0200

    table

--- src/components/workerPools.js
+++ src/components/workerPools.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+const columns = [
+  {
+    dataField: 'workerPool',
+    text: 'worker pool'
+  },
+  {
+    dataField: 'taskId',
+    text: 'task'
+  },
+  {
+    dataField: 'observed',
+    text: 'last observed'
+  },
+  {
+    dataField: '',
+    text: 'observations'
+  }
+];
 
 
 const WorkerPools = ({ workerPools }) => {
+  let data = workerPools.map(x => ({
+    workerPool: x.name,
+    taskId: x.task.id,
+    observed: x.task.created,
+    observations: x.observations
+  }));
   return (
     <div>
+      <BootstrapTable keyField='name' data={ data } columns={ columns } bootstrap4 />
       <ul>
         {
           workerPools.map((workerPool) => (
