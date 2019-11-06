4373055f34804ef8cc2ca5a687cc449840ec336c
Rob Thijssen <rthijssen@gmail.com>
2019-11-06 15:03:09 +0200

    tabled data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import WorkerPools from './components/workerPools';
+import Observations from './components/observations';
 import './App.css';
 
 const IndexApi = 'https://index.taskcluster.net/v1';
-const QueueApi = 'https://queue.taskcluster.net/v1';
+//const QueueApi = 'https://queue.taskcluster.net/v1';
 const ArtifactApi = 'https://taskcluster-artifacts.net';
 const RootNamespace = 'project.releng.a2ff8966607583fbc1944fccc256a80c';
+/*
 const versionregex = {
   'windows-version': /(\d+\.\d+\.\d+)/gm,
   'python2-version': /(\d+\.\d+\.\d+)/gm,
@@ -17,52 +18,30 @@ const versionregex = {
   'generic-worker-version': /(\d+\.\d+\.\d+)/gm,
   'servicepack-major-version': /\d/g,
 }
+*/
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      workerPools: []
+      observations: {}
     };
   }
   componentDidMount () {
-    fetch(IndexApi + '/namespaces/' + RootNamespace)
-    .then(response => response.json())
-    .then((containerPrimary) => {
-      containerPrimary.namespaces.map((ns1) => (
-        fetch(IndexApi + '/namespaces/' + ns1.namespace)
-        .then(response => response.json())
-        .then((containerSecondary) => {
-          containerSecondary.namespaces.forEach(async (ns2) => {
-            let taskId = await fetch(IndexApi + '/task/' + ns2.namespace + '.collect-software-versions').then(responseTask => responseTask.json()).then(containerTask => containerTask.taskId).catch(console.log);
-            let task = await fetch(QueueApi + '/task/' + taskId).then(responseTask => responseTask.json()).catch(console.log);
-            task.id = taskId;
-            let workerPool = {
-              name: ns1.name + '/' + ns2.name,
-              namespace: ns2.namespace,
-              task: task,
-              observations: []
-            };
-            task.payload.artifacts.forEach(async artifact => {
-              workerPool.observations.push({
-                name: artifact.name.split('/').pop().replace('.txt', ''),
-                value: (await fetch(ArtifactApi + '/' + taskId + '/0/' + artifact.path).then(responseTask => responseTask.text()).catch(console.log)).match(versionregex[artifact.name.split('/').pop().replace('.txt', '')])
-              });
-            });
-            this.setState(currentState => ({
-              workerPools: [...currentState.workerPools, workerPool]
-            }));
-          });
-
-        })
-        .catch(console.log)
-      ));
-    })
-    .catch(console.log);
+    fetch(IndexApi + '/task/' + RootNamespace + '.decision')
+    .then(namespaceResponse => namespaceResponse.json())
+    .then(namespaceContainer => {
+      fetch(ArtifactApi + '/' + namespaceContainer.taskId + '/0/public/results.json')
+      .then(observationsResponse => observationsResponse.json())
+      .then(observations => {
+        this.setState({ observations: observations });
+      })
+      .catch(console.log)})
+    .catch(console.log)
   };
   render() {
     return (
-      <WorkerPools workerPools={this.state.workerPools} />
+      <Observations observations={this.state.observations} />
     );
   }
 }
---
+++ src/components/observations.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+const Observations = ({ observations }) => {
+  var columns = [
+    {
+      dataField: 'workerType',
+      text: 'worker type',
+      sort: true
+    }
+  ];
+  let data = Object.keys(observations).map(workerType => {
+    let blob = { workerType: workerType };
+    Object.keys(observations[workerType]['collect-software-versions']['iteration-1']).forEach(key => {
+      if (key.includes('-version')) {
+        blob[key] = observations[workerType]['collect-software-versions']['iteration-1'][key];
+        if (!(columns.some(column => column.dataField === key))) {
+          columns.push({
+            dataField: key,
+            text: key.replace('-version', '').replace('-major', '').replace('-', ' '),
+            sort: true
+          });
+        }
+      }
+    });
+    return blob;
+  });
+  return (
+    <div>
+      <BootstrapTable keyField='workerType' data={ data } columns={ columns } bootstrap4 />
+    </div>
+  )
+};
+
+export default Observations
