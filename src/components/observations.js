d4a38c2d12d47b5858d04d06c9188133132e483e
Rob Thijssen <rthijssen@gmail.com>
2019-11-06 16:28:44 +0200

    bootstrap config

--- src/components/observations.js
+++ src/components/observations.js
@@ -3,7 +3,7 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 const Observations = ({ observations }) => {
-  var columns = [
+  var versionColumns = [
     {
       dataField: 'time',
       text: 'last observed',
@@ -20,7 +20,7 @@ const Observations = ({ observations }) => {
       sort: true
     }
   ];
-  let data = Object.keys(observations).map(workerType => {
+  let versionData = Object.keys(observations).map(workerType => {
     let blob = {
       workerType: workerType,
       time: observations[workerType]['collect-software-versions']['iteration-1'].task.runs[0].resolved,
@@ -29,8 +29,8 @@ const Observations = ({ observations }) => {
     Object.keys(observations[workerType]['collect-software-versions']['iteration-1']).forEach(key => {
       if (key.includes('-version')) {
         blob[key] = observations[workerType]['collect-software-versions']['iteration-1'][key];
-        if (!(columns.some(column => column.dataField === key))) {
-          columns.push({
+        if (!(versionColumns.some(column => column.dataField === key))) {
+          versionColumns.push({
             dataField: key,
             text: key.replace('-version', '').replace('-major', '').replace('-', ' '),
             sort: true
@@ -40,9 +40,62 @@ const Observations = ({ observations }) => {
     });
     return blob;
   });
+  var configColumns = [
+    {
+      dataField: 'time',
+      text: 'last observed',
+      sort: true
+    },
+    {
+      dataField: 'worker',
+      text: 'observed on',
+      sort: true
+    },
+    {
+      dataField: 'workerType',
+      text: 'worker type',
+      sort: true
+    }
+  ];
+  let configData = Object.keys(observations).map(workerType => {
+    let taskname = observations[workerType].hasOwnProperty('collect-ronin-config')
+      ? 'collect-ronin-config'
+      : observations[workerType].hasOwnProperty('collect-occ-config')
+        ? 'collect-occ-config'
+        : 'undefined';
+    let blob = {};
+    if (taskname !== 'undefined') {
+      blob = {
+        workerType: workerType,
+        time: observations[workerType][taskname]['iteration-1'].task.runs[0].resolved,
+        worker: observations[workerType][taskname]['iteration-1'].task.runs[0].workerId
+      };
+      Object.keys(observations[workerType][taskname]['iteration-1']).forEach(key => {
+        if (key.includes('-source-')) {
+          blob[key.replace('occ-', ' ').replace('ronin-', ' ')] = observations[workerType][taskname]['iteration-1'][key];
+          if (!(configColumns.some(column => column.dataField === key.replace('occ-', ' ').replace('ronin-', ' ')))) {
+            configColumns.push({
+              dataField: key.replace('occ-', ' ').replace('ronin-', ' '),
+              text: key.replace('occ-', ' ').replace('ronin-', ' ').replace('-', ' '),
+              sort: true
+            });
+          }
+        }
+      });
+    } else {
+      blob = {
+        workerType: workerType
+      };
+    }
+    return blob;
+  });
   return (
     <div>
-      <BootstrapTable keyField='workerType' data={ data } columns={ columns } bootstrap4 />
+      <h2>software version observations</h2>
+      <BootstrapTable keyField='workerType' data={ versionData } columns={ versionColumns } bootstrap4 />
+
+      <h2>bootstrap config observations</h2>
+      <BootstrapTable keyField='workerType' data={ configData } columns={ configColumns } bootstrap4 />
     </div>
   )
 };
